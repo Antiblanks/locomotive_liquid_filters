c2ab01ed646529b5c81032d1af767435d144a7d8
Daniel Ivanovic <dan.ivanovic@antiblanks.com>
2016-02-18 00:55:10 +0000

    ADDED: Imported deps

--- lib/locomotive_liquid_filters.rb
+++ lib/locomotive_liquid_filters.rb
@@ -1,3 +1,7 @@
+require 'solid'
+require 'pony'
+require 'net/http'
+
 %w{filters}.each do |dir|
   Dir[File.join(File.dirname(__FILE__), 'locomotive_liquid_filters', dir, '*.rb')].each { |lib| require lib }
 end
